6c6b532af3648ccc3d6a463b1d9c690f0a5bea47
Alfredo <atakori@outlook.com>
2017-10-28 17:00:35 -0400

    added app.js with mock data

---
+++ app.js
@@ -0,0 +1,118 @@
+let MOCK_CREATED_CLASSES = {
+	"createdClasses:": [
+		{
+			"id": "111111",
+			"className": "Ebb and Flow",
+			"genre": "Modern",
+			"difficulty": "Intermediate",
+			"choreographer": "Alfredo Takori",
+			"studio": {
+				"name": "Dance101",
+				"address": {
+					"street": "123 dance avenue" ,
+					"city": "Atlanta" ,
+					"state": "GA",
+					"zipcode": "30329"
+				},
+			},
+			"weeklyDayandTime": "Tuesdays at 8PM",
+			"dateCreated": "10/28/2017",
+			"description": "If you want a good workout, take this class!"
+		},
+		{
+			"id": "222222",
+			"className": "Discover Hip Hop"
+			"genre": "Hip Hop",
+			"difficulty": "Beginner",
+			"choreographer": "Alfredo Takori",
+			"studio": {
+				"name": "Dance101",
+				"address": {
+					"street": "123 dance avenue" ,
+					"city": "Atlanta" ,
+					"state": "GA",
+					"zipcode": "30329"
+				},
+			},
+			"weeklyDayandTime": "Saturdays at 2PM",
+			"dateCreated": "02/24/2017",
+			"description": "Perfect for beginners!"
+		},
+		{
+			"id": "333333",
+			"className": "Breaking it down",
+			"genre": "Hip Hop",
+			"difficulty": "Advanced",
+			"choreographer": "Chealsea Stevens",
+			"studio": {
+				"name": "Steven Studios",
+				"address": {
+					"street": "898 hip street",
+					"city": "Austin",
+					"state": "TX",
+					"zipcode": "73301"
+				},
+			},
+			"weeklyDayandTime": "Thursdays at 12PM",
+			"dateCreated": "10/28/2017",
+			"description": "Only the strong survive in this class!"
+		},
+		{
+			"id": "444444",
+			"className": "Slide and Tap"
+			"genre": "Tap",
+			"difficulty": "Intermediate",
+			"choreographer": "Eliza Shumucker",
+			"studio": {
+				"name": "Gotta Dance ATL"
+				"address": {
+					"street": "1879 tip tap avenue",
+					"city": "Atlanta",
+					"state": "GA",
+					"zipcode": "30328"
+				},
+			},
+			"weeklyDayandTime": "Mondays at 3PM",
+			"dateCreated": "05/22/2018",
+			"description": "Make sure you bring your tap shoes!"
+		},
+		{
+			"id": "555555",
+			"className": "Full Out"
+			"genre": "Ballet",
+			"difficulty": "Professional",
+			"choreographer": "Sarah Hilmer",
+			"studio": {
+				"name": "The Ballet Institute": ,
+				"address": {
+					"street": "5463 Alastair Road",
+					"city": "New York",
+					"state": "NY",
+					"zipcode": "12764"
+				},
+			},
+			"weeklyDayandTime": "Mondays at 10AM",
+			"dateCreated": "06/07/2019",
+			"description": "Barre, tehcnique, and choreography all in one class."
+		}
+	];
+}
+
+function getCreatedClasses(callback) {
+	setTimeout(function() { callbackfunction(createdClasses)}, 100);
+}
+
+function displaycreatedClasses(data) {
+	//need to add on click for search button
+	for (index in data.createdClasses) {
+		$('.class_results_page').html(`<li class = "class_name"> ${data.className} |
+			 ${data.genre} | ${data.studio.name} | ${data.weeklyDayandTime}
+			</li>`)
+	}
+}
+
+function getAndDisplayCreatedClasses () {
+	getCreatedClasses(displaycreatedClasses);
+}
+
+$(getAndDisplayCreatedClasses)
