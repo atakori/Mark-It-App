37948a13692e25f3baa78bf511bb60b3bfabbb72
Alfredo <atakori@outlook.com>
2017-10-30 19:44:13 -0400

    added user model.js

--- server.js
+++ server.js
@@ -1,6 +1,53 @@
 var express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+
 var app = express();
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
 
-module.exports = {app}
+
+let server;
+
+function runServer() {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(DATABASE_URL, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app
+                .listen(PORT, () => {
+                    console.log(`Your app is listening on port ${PORT}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
