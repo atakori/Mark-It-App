b367191fc006f63e098c62b3f16f4d1bba15003f
Alfredo <atakori@outlook.com>
2017-10-31 17:57:04 -0400

    added tests /userHome, /login, /searchclasses, and /upload

--- test/test-server.js
+++ test/test-server.js
@@ -7,7 +7,7 @@ const {app} = require('../server');
 
 chai.use(chaiHttp);
 
-describe('TEST endpoint', function() {
+describe('Testing ROUTES endpoints', function() {
 	it('should return HTML and 200 status code for the index.html page', function() {
 		return chai.request(app)
 		.get('/')
@@ -17,9 +17,41 @@ describe('TEST endpoint', function() {
 		});
 	});
 
+	it('should return HTML and 200 status code for the login.html page', function() {
+		return chai.request(app)
+		.get('/login')
+		.then(function(res) {
+			res.should.have.status(200);
+			res.should.be.html;
+		})
+	})
 	it('should return HTML and 200 status code for the loggedIn.html page', function() {
 		return chai.request(app)
-		.get('/loggedIn.html')
+		.get('/userHome')
+		.then(function(res) {
+			res.should.have.status(200);
+			res.should.be.html;
+		})
+	})
+	it('should return HTML and 200 status code for the search.html page', function() {
+		return chai.request(app)
+		.get('/searchclasses')
+		.then(function(res) {
+			res.should.have.status(200);
+			res.should.be.html;
+		})
+	})
+	it('should return HTML and 200 status code for the uploadVideo.html page', function() {
+		return chai.request(app)
+		.get('/upload')
+		.then(function(res) {
+			res.should.have.status(200);
+			res.should.be.html;
+		})
+	})
+	it('should return HTML and 200 status code for the signup.html page', function() {
+		return chai.request(app)
+		.get('/signup')
 		.then(function(res) {
 			res.should.have.status(200);
 			res.should.be.html;
