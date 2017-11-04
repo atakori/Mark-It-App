80a27f01ce7c631a21d8ade256b937d88cf52a7d
Alfredo <atakori@outlook.com>
2017-11-04 16:47:07 -0400

    added createClass.html

---
+++ models/classes.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const classSchema = mongoose.Schema({
+	className: {
+		type:String,
+		required: true,
+	},
+	genre: {
+		type:String,
+		required:true,
+	},
+	difficulty: {type: String},
+    choreographer: {type: String}
+    studio: {
+    	name: {type:String},
+    	address: {
+				street: {type:String},
+				city: {type:String} ,
+				state: {type:String},
+				zipcode: {type:String}
+			}
+    	}
+    weeklyDayandTime: {
+		type:String,
+		required: true,
+	},
+	dateCreated: {type: Date, default: Date.now}
+	description: {type:String},
+	currentUsers: [String],
+	currentVideos: [String]
+	})
+
+classSchema.virtual('addressString').get(function() {
+  return `${this.address.street} ${this.address.city}, 
+  ${this.address.state} ${this.address.zipcode}`.trim()});
+
+classSchema.methods.apiRepr = function() {
+    return {
+    	id:this._id,
+        className: this.username,
+        genre: this.genre,
+        difficulty: this.difficulty,
+        choreographer: this.genre,
+        studioAddress: this.addressString,
+        weeklyDayandTime: this. weeklyDayandTime,
+        dateCreated: this.dateCreated,
+        description: this.description,
+        currentUsers: this.currentUsers,
+        currentVideos: this.currentVideos
+    };
+};
+
+const Class = mongoose.model('Class', ClassSchema);
+
+module.exports = {Class};
