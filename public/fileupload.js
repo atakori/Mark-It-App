75ffbc00adce79b991388a120e225ba56033eee2
Alfredo <atakori@outlook.com>
2017-11-08 21:50:22 -0500

    added video progress bar

--- public/fileupload.js
+++ public/fileupload.js
@@ -1,19 +1,36 @@
 function handleVideoUpload (){
-$('.upload_form').append($.cloudinary.unsigned_upload_tag("o8uzrarh", 
+$('.video-info').append($.cloudinary.unsigned_upload_tag("o8uzrarh", 
   { cloud_name: 'mark-it-cloud' }).bind('cloudinaryprogress', function(e, data) { 
-
   $('.progress_bar').css('width', 
     Math.round((data.loaded * 100.0) / data.total) + '%'); 
+  
+  if((Math.round((data.loaded * 100.0) / data.total) + '%') === "100%") {
+  	$('.progress_bar').html('Video Ready to be uploaded');
+  	$('.progress_bar').css('background-color', 'green');
+  	console.log("done");
+  } else if ((Math.round((data.loaded * 100.0) / data.total) + '%') === undefined) {
+	$('.progress_bar').html('Please select a video to upload');
+  	$('.progress_bar').css('background-color', 'none');
+  } else {
+  	$('.progress_bar').html('Video Loading');
+  	$('.progress_bar').css('background-color', 'red');
+  }
 }))
+console.log($(".progress_bar").widthPerc());
 }
 
-$(handleVideoUpload());
-/*$(document).ready(function() {
-  if($.fn.cloudinary_fileupload !== undefined) {
-    $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
-  }
-});
+function handleUserSelectedVideo() {
+	$('input').on('click', '.cloudinary_fileupload', function(e) {
+		$('.upload_video_page').append(`<div class= "progress_bar_border"> 
+					<p class="progress_bar"></p>
+			</div>`)
+	})
+}
 
-$('.cloudinary-fileupload').bind('cloudinaryprogress', function(e, data) { 
-    $('.progress_bar').css('width', Math.round((data.loaded * 100.0) / data.total) + '%');});
-*/
+$.fn.widthPerc = function(){
+    var parent = this.parent();
+    return ~~((this.width()/parent.width())*100)+"%";
+}
+
+$(handleUserSelectedVideo());
+$(handleVideoUpload());
