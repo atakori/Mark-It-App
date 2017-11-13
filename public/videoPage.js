ff04318f5f871cff47544262909dd195c799d90a
Alfredo <atakori@outlook.com>
2017-11-13 00:37:00 -0500

    implemented video.js for rendering videos with html5

--- public/videoPage.js
+++ public/videoPage.js
@@ -16,6 +16,10 @@ function displayVideoInformation(data) {
 	for (let i=0; i<data.videos.length; i++) {
 		if(data.videos[i].video_id === id) {
 			let video_object = data.videos[i];
+			$('.class_video').html(`<video id="my_video_1" class="video-js vjs-default-skin" width="640px" height="267px"
+      controls preload="none" poster="https://res.cloudinary.com/mark-it-cloud/video/upload/${video_object.video_id}.jpeg"
+      data-setup='{ "aspectRatio":"640:267", "playbackRates": [1, 1.5, 2] }'> 
+      <source src="${video_object.video_url}" type='video/mp4' /></video>`)
 			$('.video_info_section').html(`<h2> showing video Title!!! </h2>
 				<p class= "video_title">${video_object.videoTitle} </p>`);
 		}
